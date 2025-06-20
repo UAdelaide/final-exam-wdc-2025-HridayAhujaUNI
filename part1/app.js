ab3ccde3f9a1996096d65163bd0c04ffa8b2e879
HridayAhujaUNI <hriday.ahuja@student.adelaide.edu.au>
2025-06-20 18:23:45 +0930

    b

--- part1/app.js
+++ part1/app.js
@@ -5,8 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-
 var app = express();
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
