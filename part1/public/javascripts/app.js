53f3dff83b32163bcd81e47a7a76dc7f6aa74fe5
HridayAhujaUNI <hriday.ahuja@student.adelaide.edu.au>
2025-06-20 12:16:36 +0000

    [automatic commit] 2025-06-20 12:16:35 UTC 220.233.8.2

--- part1/public/javascripts/app.js
+++ part1/public/javascripts/app.js
@@ -1,19 +1,19 @@
 const { createApp } = Vue;
 createApp({
-    data() {
-        return {
-            dogImage: '',
-            dogName: 'MC Dog'
-        };
-    },
-    methods: {
-        async fetchDog() {
-            const res = await fetch('https://dog.ceo/api/breeds/image/random');
-            const data = await res.json();
-            this.dogImage = data.message;
-        }
-    },
-    mounted() {
-        this.fetchDog();
+  data() {
+    return {
+      dogImage: '',
+      dogName: 'MC Dog'
+    };
+  },
+  methods: {
+    async fetchDog() {
+      const res = await fetch('https://dog.ceo/api/breeds/image/random');
+      const data = await res.json();
+      this.dogImage = data.message;
     }
+  },
+  mounted() {
+    this.fetchDog();
+  }
 }).mount('#app');
