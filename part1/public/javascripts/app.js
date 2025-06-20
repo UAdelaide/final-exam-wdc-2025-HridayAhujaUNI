b4e5b11f30fa415c2b6257956d0c0127aa1a5e4b
HridayAhujaUNI <hriday.ahuja@student.adelaide.edu.au>
2025-06-20 08:57:38 +0000

    [automatic commit] 2025-06-20 08:57:38 UTC 220.233.8.2

--- part1/public/javascripts/app.js
+++ part1/public/javascripts/app.js
@@ -11,7 +11,7 @@ createApp({
             const res = await fetch('https://dog.ceo/api/breeds/image/random');
             const data = await res.json();
             this.dogImage = data.message;
-        },
+        }
     },
     mounted() {
         this.fetchDog();
